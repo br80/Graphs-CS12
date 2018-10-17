e997dd056604bad27c91254ab7eaa97df01dc9f3
Brady Fukumoto <bradyfukumoto@gmail.com>
2018-10-17 10:51:56 -0700

    Adding labels to nodes

--- projects/graph/src/draw.py
+++ projects/graph/src/draw.py
@@ -2,7 +2,7 @@ import math
 
 from bokeh.io import show, output_file
 from bokeh.plotting import figure
-from bokeh.models import GraphRenderer, StaticLayoutProvider, Oval, Circle
+from bokeh.models import GraphRenderer, StaticLayoutProvider, Oval, Circle, ColumnDataSource, Label, LabelSet
 from bokeh.palettes import Spectral8
 from graph import Graph
 
@@ -53,6 +53,17 @@ class BokehGraph:
         graph_renderer.layout_provider = StaticLayoutProvider(graph_layout=graph_layout)
 
         plot.renderers.append(graph_renderer)
+
+        labelSource = ColumnDataSource(data=dict(x=x, y=y, names=[vertex_id for vertex_id in graph.vertices]))
+        labels = LabelSet(x='x', y='y', text='names', level='glyph',
+                     text_align='center', text_baseline='middle', source=labelSource, render_mode='canvas')
+
+
+        plot.add_layout(labels)
+
+
+
+
 
         output_file('graph.html')
         show(plot)
