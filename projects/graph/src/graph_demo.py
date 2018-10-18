4a4fccf02b26cf92742fb3b7b935a8a25c1376f5
Brady Fukumoto <bradyfukumoto@gmail.com>
2018-10-18 11:10:33 -0700

    BFS/DFS/BFT/DFT code

--- projects/graph/src/graph_demo.py
+++ projects/graph/src/graph_demo.py
@@ -12,15 +12,24 @@ import random
 
 def createDefaultGraph():
     graph = Graph()  # Instantiate your graph
-    graph.add_vertex('0')
-    graph.add_vertex('1')
-    graph.add_vertex('2')
-    graph.add_vertex('3')
-    graph.add_edge('0', '1')
-    graph.add_edge('0', '3')
+    graph.add_vertex(5)
+    graph.add_vertex(2)
+    graph.add_vertex(6)
+    graph.add_vertex(1)
+    graph.add_vertex(4)
+    graph.add_vertex(7)
+    graph.add_vertex(3)
+    graph.add_edge(5, 2)
+    graph.add_edge(5, 6)
+    graph.add_edge(2, 1)
+    graph.add_edge(2, 4)
+    graph.add_edge(4, 3)
+    graph.add_edge(6, 7)
 
-    bg = BokehGraph(graph)
-    bg.draw()
+    print(graph.dfs(5, 7))
+
+    # bg = BokehGraph(graph)
+    # bg.draw()
 
 
 # O(n^2)
